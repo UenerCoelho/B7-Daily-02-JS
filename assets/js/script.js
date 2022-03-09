15222b90422e93c43dc1d65345afbc01948cd22e
Uener Coelho <uenercoelho2@hotmail.com>
2022-03-08 22:59:28 -0400

    JS Lesson

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Variáveis
+let n1 = 10
+let n2 = 5
+
+let total = n1 + n2
+
+console.log('O total foi: ' + total)
+
+// Condicional
+if (total > 50) {
+  console.log('Total foi MAIOR que 50')
+} else {
+  console.log('Total foi MENOR que 50')
+}
