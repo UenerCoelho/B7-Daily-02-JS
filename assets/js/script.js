4fd74e6d5371488dcf59e359144487e2336c01cd
Uener Coelho <uenercoelho2@hotmail.com>
2022-03-09 23:07:40 -0400

    Js lessons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,20 @@
 let n1 = 10
 let n2 = 5
 
-let total = n1 + n2
+//Array
+let ingrediente = [
+  '2 xícaras (chá) de açúcar',
+  '3 xícaras (chá) de farinha de trigo',
+  '4 colheres (sopa) de margarina',
+  '3 ovos',
+  '1 e 1/2 cícara (chá) de leite',
+  '1 colher (sopa) bem cheia de fermento em pó',
+  '100ml de corante azul'
+]
 
-console.log('O total foi: ' + total)
-
-// Condicional
-if (total > 50) {
-  console.log('Total foi MAIOR que 50')
-} else {
-  console.log('Total foi MENOR que 50')
-}
+// // Condicional
+// if (total > 50) {
+//   console.log('Total foi MAIOR que 50')
+// } else {
+//   console.log('Total foi MENOR que 50')
+// }
